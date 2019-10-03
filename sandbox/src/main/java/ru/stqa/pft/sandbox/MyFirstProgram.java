60ba0663c7422321c865880ad6b715fadc5060c5
deidrah <kumkwaczka@gmail.com>
2019-10-03 20:27:23 +0200

    Added a few funcions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,28 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        hello("world");
+        hello("Asia");
+        hello("Kasia");
+
+        double l = 5;
+        System.out.println("Pole kwadratu o boku " + l + " = " + area(1));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Pole prostokąta o bokach " + a + " i " + b + " = " + area(a,b));
     }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double len) {
+        return len * len;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
+    }
+
 }
