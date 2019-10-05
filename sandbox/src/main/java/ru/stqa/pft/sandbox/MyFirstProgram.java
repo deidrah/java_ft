8015e121405ab7812b9908f9e00086e0ea9bcc6c
deidrah <kumkwaczka@gmail.com>
2019-10-05 22:00:43 +0200

    made some refactoration of the code

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -11,10 +11,9 @@ public class MyFirstProgram {
         Rectangle r = new Rectangle(4,6);
         System.out.println("Pole prostokąta o bokach " + r.a + " i " + r.b + " = " + r.area());
 
-        Point p = new Point ();
         Point p1 = new Point(10, 12);
         Point p2 = new Point(14, 2);
-        System.out.println("Odległość między punktem p1 i p2 wynosi " + p.distance(p1, p2));
+        System.out.println("Odległość między punktem p1 i p2 wynosi " + p1.distance(p2));
     }
 
     public static void hello(String somebody) {
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -4,19 +4,14 @@ public class Point {
     public double x;
     public double y;
 
-    public Point() {
-        this.x=0.0;
-        this.y=0.0;
-    }
-
     public Point(double x, double y) {
         this.x=x;
         this.y=y;
     }
 
-    public double distance(Point p1, Point p2){
-        double dx = p1.x - p2.x;
-        double dy = p1.y - p2.y;
+    public double distance(Point p1){
+        double dx = p1.x - this.x;
+        double dy = p1.y - this.y;
         return Math.sqrt(dx * dx + dy * dy);
     }
 }
