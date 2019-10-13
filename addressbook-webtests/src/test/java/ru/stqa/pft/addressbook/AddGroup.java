2788472724110907b5d632ad1b17c742a2bf35ac
deidrah <kumkwaczka@gmail.com>
2019-10-13 20:20:52 +0200

    Code refactoring for AddGroup

--- addressbook-webtests/src/test/java/ru/stqa/pft/addressbook/AddGroup.java
+++ addressbook-webtests/src/test/java/ru/stqa/pft/addressbook/AddGroup.java
@@ -13,32 +13,60 @@ public class AddGroup {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://addressbook.pl/addressbook/group.php");
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(username);
     wd.findElement(By.name("pass")).click();
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
   }
 
   @Test
   public void testGroupCreation() throws Exception {
-    wd.get("http://addressbook.pl/addressbook/");
-    wd.findElement(By.xpath("//img[@alt='Addressbook']")).click();
-    wd.findElement(By.linkText("groups")).click();
-    wd.findElement(By.name("new")).click();
+    goToMainPage();
+    gotToGroupPage();
+    groupAddingInit();
+    fillingGroupForm(new GroupData("Test", "Test", "Test"));
+    submitGroupForm();
+    returnToGroupPage();
+  }
+
+  private void returnToGroupPage() {
+    wd.findElement(By.linkText("group page")).click();
+  }
+
+  private void submitGroupForm() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+  private void fillingGroupForm(GroupData groupData) {
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys("Test");
+    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
     wd.findElement(By.name("group_header")).click();
     wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys("Test");
+    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
     wd.findElement(By.name("group_footer")).click();
     wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys("Test");
-    wd.findElement(By.name("submit")).click();
-    wd.findElement(By.linkText("group page")).click();
+    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+  }
+
+  private void groupAddingInit() {
+    wd.findElement(By.name("new")).click();
+  }
+
+  private void gotToGroupPage() {
+    wd.findElement(By.linkText("groups")).click();
+  }
+
+  private void goToMainPage() {
+    wd.get("http://addressbook.pl/addressbook/");
+    wd.findElement(By.xpath("//img[@alt='Addressbook']")).click();
   }
 
   @AfterMethod(alwaysRun = true)
